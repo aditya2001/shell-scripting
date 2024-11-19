b27c00ec6281c3b989b767dfd16048b8f130786c
Aditya <aditya.200187@gmail.com>
2024-11-19 23:16:53 +0530

    Added scripts

---
+++ scripts/P04_CheckErrorInLogs.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+matched_error=`grep -i error /var/log/messages`
+echo $matched_error
+if [[ $? -eq 0 ]];then
+        echo "found error in OS logs: $matched_error "
+else
+        echo "no error in message logs"
+fi
