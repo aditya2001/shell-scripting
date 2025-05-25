d50f63eecaaa233711d1215c5a010d37a8a4aa7d
Aditya Choudhary <34318776+aditya2001@users.noreply.github.com>
2025-05-24 22:22:25 -0500

    Update P02_CheckPrimeNumber.sh

--- scripts/P02_CheckPrimeNumber.sh
+++ scripts/P02_CheckPrimeNumber.sh
@@ -3,8 +3,10 @@
 echo -e "Enter Number : \c"
 read -r n
 
-for((i=2; i<$n; i++));do
-  if [ $((n%i)) -eq 0 ];then
+for((i=2; i<n; i++))
+ do
+  if [ $((n%i)) -eq 0 ]
+   then
     echo "$n is not a prime number."
     exit 0
   fi
